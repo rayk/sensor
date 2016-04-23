844e726adae14753cd0fb4b0d4d83b8fa44ccda8
rayk <taipan.king@gmail.com>
2016-04-23 18:32:00 +1000

    feature: IO connection
    Returning a stream from what is be read.

--- lib/src/connectors/filio.dart
+++ lib/src/connectors/filio.dart
@@ -1,18 +1,16 @@
-library connector_filio;
+/// Connector for a OS directory in a path.
+library connector_io_directory;
 
-import 'package:path/path.dart' as p;
 import 'dart:async';
+import 'dart:convert';
 import 'dart:io';
+import 'package:async/async.dart' show StreamGroup;
+import 'package:path/path.dart' as p;
 
-/// Function that retruns a stream with raw sensor data compliant to the
-/// exist specified in settings
-typedef Stream<String> Connection(Map<String, String> settings);
+import 'connector_types.dart';
 
-/// A function type that returns a connector to sensor.
-typedef Future<Connection> Connector(List<String> identifier);
-
-
-/// Connects to a File System Directory the must already exist.
+/// Returns a [Connection] to a File System Directory that must already exist.
+/// the connection is capable any CSV UTF-8 file
 Future<Connection> ioDirConnector(List<String> pathToDirectory) async {
   const String erMsg00 = 'Does not appears to be directory!';
   var ctx = new p.Context();
@@ -20,14 +18,23 @@ Future<Connection> ioDirConnector(List<String> pathToDirectory) async {
 
   return await FileStat.stat(targetPath).then((FileStat feses) {
     if (feses.type == FileSystemEntityType.DIRECTORY) {
+      Directory dir = new Directory(targetPath);
+      List<FileSystemEntity> fse =
+          dir.listSync(followLinks: false, recursive: false);
+          fse.retainWhere((f)=> f is File);
+      List<File> df = new List.from(fse);
 
       // Enclosure returned for the streaming of files.
-      Stream<String> connection(Map setting){
-        Directory source = new Directory(targetPath);
+      //TODO: Need work out a better way then the cast from UTF8.
+      Stream connection(){
+        List<Stream> fs = new List();
+        df.forEach((f) => fs.add(f
+            .openRead()
+            .transform(UTF8.decoder)
+            .transform(const LineSplitter())));
+        return StreamGroup.merge(fs);
       }
-
       return connection;
-
     } else {
       throw new ArgumentError('$targetPath - $erMsg00');
     }
--- test/unit/fileIO_connector_test.dart
+++ test/unit/fileIO_connector_test.dart
@@ -1,32 +1,41 @@
 @TestOn('vm')
+@Timeout(const Duration(seconds: 5))
 library unit_test_filio;
 
+import 'dart:async';
 import 'package:test/test.dart';
 import 'package:sensor/src/connectors/filio.dart';
+import 'package:sensor/src/connectors/connector_types.dart';
 
 /// Unit test of the
-void main(){
+void main() {
+  List<String> validPath = ['test', 'data', 'light'];
+  List<String> nonExistentPath = ['test', 'data', 'does_not_exist'];
+  List<String> validFileNotDirectory = ['test', 'data', 'badStubFile.csv'];
 
-  group('Connects to directory:',(){
-
-    List<String> validPath = ['test', 'data', 'light'];
-    List<String> nonExistentPath = ['test', 'data', 'does_not_exist'];
-    List<String> validFileNotDirectory = ['test', 'data', 'badStubFile.csv'];
-
-    test('Should provide a connector function.',() async {
+  group('Connects to directory:', () {
+    test('Should provide a connector function.', () async {
       Connection lightConnection = await ioDirConnector(validPath);
       expect(lightConnection, isNotNull);
       expect(lightConnection is Connection, isTrue);
     });
 
-    test("Should Error if provide a bad path.",(){
+    test("Should Error if provide a bad path.", () {
       expect(ioDirConnector(nonExistentPath), throwsArgumentError);
     });
 
-    test('Should Error if provided with a file.',(){
+    test('Should Error if provided with a file.', () {
       expect(ioDirConnector(validFileNotDirectory), throwsArgumentError);
     });
-
   });
 
+  group('Provides a string of sensor data', () {
+    test("Should provide a stream of list given vaild input", () async {
+      Connection lightSensor = await ioDirConnector(validPath);
+      Stream sensorData = lightSensor();
+      sensorData.listen(((var datum) {
+        
+      }));
+    });
+  });
 }
